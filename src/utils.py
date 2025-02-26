20628a19970e09ca0854d387696e3639d0b3d224
Hirenkumar Patel <hirenpatel4897@gmail.com>
2025-02-26 14:17:52 -0500

    Model Training

--- src/utils.py
+++ src/utils.py
@@ -21,4 +21,22 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+            
+            report[model_name] = test_score
+
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
